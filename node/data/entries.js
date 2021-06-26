4469bcc220607df3900d42bb29f85d0d1d85d31e
Firouzeh Tahbaz <firouzeh2013@yahoo.ca>
2021-06-26 14:43:34 -0400

    initial commit

---
+++ node/data/entries.js
@@ -0,0 +1,30 @@
+let entries = [
+    {
+        id: 1,
+        name: "some string",
+        email: "address@email.com",
+        phoneNumber: "2343331234",
+        contact: "User's message goes here"
+    }, 
+    {
+        id: 2,
+        name: "some string",
+        email: "address@email.com",
+        phoneNumber: "2343331234",
+        contact: "User's message goes here"
+    }, {
+        id: 3,
+        name: "some string",
+        email: "address@email.com",
+        phoneNumber: "2343331234",
+        contact: "User's message goes here"
+    }, {
+        id: 4,
+        name: "some string",
+        email: "address@email.com",
+        phoneNumber: "2343331234",
+        contact: "User's message goes here"
+    }
+ ]
+
+export default entries;
