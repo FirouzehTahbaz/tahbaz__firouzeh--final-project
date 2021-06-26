8bd80de493ebf2b4d401902feeb2347ff3bf4aa7
Firouzeh Tahbaz <firouzeh2013@yahoo.ca>
2021-06-26 14:41:21 -0400

    initial commit

---
+++ node/data/users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+      name: "User One",
+      email: "address@email.com",
+      password: "somePasswordOne",
+    },
+    {
+      name: "User Two",
+      email: "address2@email.com",
+      password: "somePasswordTwo",
+    },
+    {
+      name: "User Three",
+      email: "address3@email.com",
+      password: "somePasswordThree",
+    },
+  ];
+  
+module.exports = users
